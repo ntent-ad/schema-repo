d2471bc8ee1f3a389efc4bebd2e97f7e9a494b74
thunderstumpges <tstumpges@ntent.com>
2015-01-23 23:52:13 -0800

    remove unused import

--- jdbc/src/main/java/org/schemarepo/jdbc/JdbcRepository.java
+++ jdbc/src/main/java/org/schemarepo/jdbc/JdbcRepository.java
@@ -25,7 +25,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.Properties;
 import java.util.UUID;
 
